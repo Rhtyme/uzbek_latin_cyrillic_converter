d6b3098b0055788e105d406bc048d2e23ba232f4
rhtyme <rhtyme@gmail.com>
2022-05-03 19:33:47 +0500

    icreased version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='uzbek_latin_cyrillic_converter',
-    version='0.1',
+    version='0.2',
     license='MIT',
     author="Sirojiddin Komolov",
     author_email='rhtyme@gmail.com',
